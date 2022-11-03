32ca0a273c153372ddddb96760ec1518f662188b
maokangkun <maokangkun>
2022-11-03 14:24:29 +0800

    + training

--- src/1.py
+++ src/1.py
@@ -1,59 +1,133 @@
 from utils import *
 seed = 42
 set_metaverse_seed(seed)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = 64
+lr = 1e-3
+epochs = 500
 
 tokenizer = AutoTokenizer.from_pretrained("lucashu/TcmYiAnBERT")
-mlm_model = AutoModelForMaskedLM.from_pretrained("lucashu/TcmYiAnBERT")
-model = AutoModel.from_pretrained("lucashu/TcmYiAnBERT")
+# mlm_model = AutoModelForMaskedLM.from_pretrained("lucashu/TcmYiAnBERT").to(device)
+model = AutoModel.from_pretrained("lucashu/TcmYiAnBERT").to(device)
 
 data = pd.read_csv('../data/tcmspt.csv').fillna('未知')
 text_concat = '年龄：' + data['Age'].astype(str) + '。性别：' +data['Gender'] + '。病史：' + data['Medical History'] + '舌诊：' + data['Tongue'] + '。脉诊：' + data['Pulse'] + '。'
 feat = text_concat.to_list()
 label = data.Type.to_list()
-
-train_x, train_y, valid_x, valid_y, test_x, test_y = train_valid_test_split(feat, label)
+label_map = {k:i for i, k in enumerate(set(label))}
+label_idx = [label_map[k] for k in label]
+out_dim = len(label_map)
+in_dim = model.pooler.dense.out_features
 
 print(f'seed: {seed}')
 print(f'tokenizer: {tokenizer}')
 # print(f'model: {model}')
-print('[cyan]data[/]')
-print(f'total: {len(data)}')
+print(f'total data: {len(data)}')
 print('label distribution:')
 show_distribution(label)
+# test_example(tokenizer, mlm_model, feat)
 
-print(f'example: {feat[0]}')
-tokenized_text = tokenizer.tokenize(feat[0])
-token_idx = tokenizer.convert_tokens_to_ids(tokenized_text)
-token_tensor = torch.tensor([token_idx])
+batch_encode = tokenizer.batch_encode_plus(feat, padding=True)
+input_ids = torch.tensor(batch_encode['input_ids']).to(device)
+token_type_ids = torch.tensor(batch_encode['token_type_ids']).to(device)
+attention_mask = torch.tensor(batch_encode['attention_mask']).to(device)
+model.eval()
+with torch.no_grad():
+    out = model(input_ids, token_type_ids, attention_mask)
+    embedding = out.last_hidden_state[:,0]
+    pooled = out.pooler_output
+# train_feat, train_label, valid_feat, valid_label, test_feat, test_label = train_valid_test_split(embedding, label_idx)
+train_feat, train_label, valid_feat, valid_label, test_feat, test_label = train_valid_test_split(pooled, label_idx)
+train_data = TCMDataset(train_feat, train_label)
+valid_data = TCMDataset(valid_feat, valid_label)
+test_data = TCMDataset(test_feat, test_label)
+train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+valid_dataloader = DataLoader(valid_data, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
-encode_idx = tokenizer.encode(feat[0])
-encode_text = ''.join(tokenizer.convert_ids_to_tokens(encode_idx))
-encode_tensor = torch.tensor([encode_idx])
+class MLP(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(in_dim, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(64, out_dim),
+        )
 
-out = mlm_model(token_tensor)
-out_token_idx = out.logits[0].argmax(1).tolist()
-out_token = tokenizer.convert_ids_to_tokens(out_token_idx)
-for i in range(len(tokenized_text)):
-    if out_token[i] != tokenized_text[i]:
-        out_token[i] = '[red]' + out_token[i] + '[/]'
-out_text = ''.join(out_token)
+    def forward(self, x):
+        return self.fc(x)
 
-out2 = mlm_model(encode_tensor)
-out2_token_idx = out2.logits[0].argmax(1).tolist()
-out2_token = tokenizer.convert_ids_to_tokens(out2_token_idx)
-for i in range(len(tokenized_text)):
-    if out2_token[i+1] != tokenized_text[i]:
-        out2_token[i+1] = '[red]' + out2_token[i+1] + '[/]'
-out2_text = ''.join(out2_token)
+mlp = MLP(in_dim, out_dim).to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(mlp.parameters(), lr=lr)
 
-print(f'tokenized: {tokenized_text}')
-print(f'tokenized index: {token_idx}')
-print(f'encode index: {encode_idx}')
-print(f'encode text: {encode_text}')
-print(f'model out: {out_token_idx}')
-print(f'model out text: {out_text}')
-print(f'model out (encode): {out2_token_idx}')
-print(f'model out text: {out2_text}')
+print(f'train data: {len(train_data)}')
+print(f'valid data: {len(valid_data)}')
+print(f'test data: {len(test_data)}')
+print(f'input dim: {in_dim}')
+print(f'output dim: {out_dim}')
+print(f'batch size: {batch_size}')
+print(f'learning rate: {lr}')
+print(f'epochs: {epochs}')
+print(f'mlp: {mlp}')
+
+def train_a_epoch(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.train()
+    train_loss, correct = 0, 0
+    for batch, (x, y) in enumerate(dataloader):
+        x, y = x.to(device), y.to(device)
+
+        pred = model(x)
+        loss = loss_fn(pred, y)
+        train_loss += loss.item()
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # if batch % 10 == 0:
+        loss, current = loss.item(), batch * len(x)
+        print(f"loss: {loss:>7f}  [{current:>3d}/{size:>3d}]")
+    train_loss /= num_batches
+    correct /= size
+    return train_loss, correct
+
+def test_a_epoch(dataloader, model, loss_fn, prefix='Test'):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"{prefix} dataset:\nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
+    return test_loss, correct
+
+result = {'train': [[],[]], 'valid': [[],[]], 'test': [[],[]]}
+for epoch in range(epochs):
+    print(f"\nEpoch {epoch+1}\n-------------------------------")
+    train_loss, train_acc = train_a_epoch(train_dataloader, mlp, loss_fn, optimizer)
+    valid_loss, valid_acc = test_a_epoch(valid_dataloader, mlp, loss_fn, 'Valid')
+    test_loss, test_acc = test_a_epoch(test_dataloader, mlp, loss_fn)
+    result['train'][0].append(train_loss)
+    result['train'][1].append(train_acc)
+    result['valid'][0].append(valid_loss)
+    result['valid'][1].append(valid_acc)
+    result['test'][0].append(test_loss)
+    result['test'][1].append(test_acc)
+
+plot(result, 'mlp-4')
 
 # breakpoint()
-
--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,11 @@ import random
 import collections
 import numpy as np
 import pandas as pd
+import torch.nn as nn
+import matplotlib.pyplot as plt
 from rich.console import Console
 from rich.traceback import install
+from torch.utils.data import Dataset, DataLoader
 # from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, AutoModelForMaskedLM, AutoModel
 
@@ -63,8 +66,8 @@ def train_valid_test_split(x, y, train_ratio=0.8, valid_ratio=0.1):
     idxs = list(range(n))
     random.shuffle(idxs)
     train_idxs = idxs[:int(n*train_ratio)]
-    valid_idxs = idxs[int(n*train_ratio):int(n*valid_ratio)]
-    test_idxs = idxs[int(n*valid_ratio):]
+    valid_idxs = idxs[int(n*train_ratio):int(n*(train_ratio+valid_ratio))]
+    test_idxs = idxs[int(n*(train_ratio+valid_ratio)):]
 
     train_x, train_y, valid_x, valid_y, test_x, test_y = [], [], [], [], [], []
     for i in train_idxs:
@@ -77,3 +80,86 @@ def train_valid_test_split(x, y, train_ratio=0.8, valid_ratio=0.1):
         test_x.append(x[i])
         test_y.append(y[i])
     return train_x, train_y, valid_x, valid_y, test_x, test_y
+
+def test_example(tokenizer, mlm_model, feat, index=0):
+    example = feat[index]
+    print(f'example: {example}')
+    tokenized_text = tokenizer.tokenize(example)
+    token_idx = tokenizer.convert_tokens_to_ids(tokenized_text)
+    token_tensor = torch.tensor([token_idx])
+
+    encode_idx = tokenizer.encode(example)
+    encode_text = ''.join(tokenizer.convert_ids_to_tokens(encode_idx))
+    encode_tensor = torch.tensor([encode_idx])
+
+    encode_out = tokenizer.batch_encode_plus(feat, padding=True)
+    encode_idx2 = encode_out['input_ids'][index]
+    encode_text2 = ''.join(tokenizer.convert_ids_to_tokens(encode_idx2))
+    encode_tensor2 = torch.tensor([encode_idx2])
+    token_type_ids_tensor = torch.tensor([encode_out['token_type_ids'][index]])
+    attention_mask_tensor = torch.tensor([encode_out['attention_mask'][index]])
+
+    out = mlm_model(token_tensor)
+    out_token_idx = out.logits[0].argmax(1).tolist()
+    out_token = tokenizer.convert_ids_to_tokens(out_token_idx)
+    for i in range(len(tokenized_text)):
+        if out_token[i] != tokenized_text[i]:
+            out_token[i] = '[red]' + out_token[i] + '[/]'
+    out_text = ''.join(out_token)
+
+    out2 = mlm_model(encode_tensor)
+    out2_token_idx = out2.logits[0].argmax(1).tolist()
+    out2_token = tokenizer.convert_ids_to_tokens(out2_token_idx)
+    for i in range(len(tokenized_text)):
+        if out2_token[i+1] != tokenized_text[i]:
+            out2_token[i+1] = '[red]' + out2_token[i+1] + '[/]'
+    out2_text = ''.join(out2_token)
+
+    out3 = mlm_model(encode_tensor2, token_type_ids=token_type_ids_tensor, attention_mask=attention_mask_tensor)
+    out3_token_idx = out3.logits[0].argmax(1).tolist()
+    out3_token = tokenizer.convert_ids_to_tokens(out3_token_idx)
+    for i in range(len(tokenized_text)):
+        if out3_token[i+1] != tokenized_text[i]:
+            out3_token[i+1] = '[red]' + out3_token[i+1] + '[/]'
+    out3_text = ''.join(out3_token)
+
+    print(f'tokenized: {tokenized_text}')
+    print(f'tokenized index: {token_idx}')
+    print(f'encode index: {encode_idx}')
+    print(f'encode text: {encode_text}')
+    print(f'batch encode index ({len(encode_idx2)}): {encode_idx2}')
+    print(f'batch encode text ({len(encode_idx2)}): {encode_text2}')
+    print(f'model out: {out_token_idx}')
+    print(f'model out text: {out_text}')
+    print(f'model out (encode): {out2_token_idx}')
+    print(f'model out text: {out2_text}')
+    print(f'model out (batch): {out2_token_idx}')
+    print(f'model out text: {out2_text}')
+
+class TCMDataset(Dataset):
+    def __init__(self, feat, label):
+        self.feat = feat
+        self.label = label
+
+    def __len__(self):
+        return len(self.feat)
+
+    def __getitem__(self, idx):
+        feat = self.feat[idx]
+        label = self.label[idx]
+        return feat, label
+
+def plot(result, out):
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+
+    for k in result:
+        ax1.plot(result[k][0], label=k)
+        ax1.set_title('Loss')
+        ax1.legend()
+
+        ax2.plot(result[k][1], label=k)
+        ax2.set_title('Accuracy')
+        ax2.legend()
+
+    plt.tight_layout()
+    plt.savefig(f'log/{out}.png', dpi=300)
